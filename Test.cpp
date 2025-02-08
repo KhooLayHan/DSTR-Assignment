f7fa567c8a469c7f67c23ff7f325fa0990f914c1
KhooLayHan <KhooLayHan@github.com>
2025-02-08 21:41:04 +0800

    Updated Logger class and separated File/Console handler

---
+++ Test.cpp
@@ -0,0 +1,19 @@
+// #include <string>
+
+// #include "Test.h"
+
+// namespace PerformanceEvaluation {
+
+//     void Test::AddTest() {
+            
+//         }
+        
+//     std::string Test::AddTest() const {
+
+//     }
+    
+//     void Test::AddTest(std::string string_) {
+
+//     }
+
+//  }
