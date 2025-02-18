154b332f4c1900a17a5541b6cc6f22808c2fe931
KhooLayHan <KhooLayHan@github.com>
2025-02-18 11:21:52 +0800

    Change Algorithm

---
+++ ConfigManager.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#ifndef CONFIG_MANAGER_HPP
+#define CONFIG_MANAGER_HPP
+
+#include <string>
+#include <string_view>
+
+namespace PerformanceEvaluation {
+    class ConfigManager {
+        public:
+            ConfigManager(int argc, char** argv);
+
+            void LoadFromArgs(int argc, char** argv);
+
+            std::string Get     (std::string_view key, std::string_view placeholder) const;
+            int         GetInt  (std::string_view key, std::string_view placeholder) const;
+            bool        GetBool (std::string_view key, std::string_view placeholder) const;
+
+            void Set(std::string_view key, std::string_view value);
+
+        private:
+
+    };
+} // namespace PerformanceEvaluation
+
+#endif 
